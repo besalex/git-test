a00f715b20afa0e407d4fe5ea6b66967ff2c816c
besalex <besalex@live.ru>
2020-11-12 23:44:19 +0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using StudioRMLib;
+using StudioCoreLib;
+
+namespace p1
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			IDmApplication rm = new StudioRMLib.DmApplication();
+			rm.ControlBars.Output.WriteLine("Hello from Mono!");
+
+			Console.WriteLine("Hello World!");
+
+		}
+	}
+}
